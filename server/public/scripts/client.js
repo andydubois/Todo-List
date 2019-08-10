4694d0b01e4a8f766a05e6785159f3f09044ea65
Andy DuBois <andydubois89@gmail.com>
2019-08-10 14:30:50 -0500

    todo list appends to DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -13,7 +13,29 @@ function getList() {
         url: '/todo'
     }).then(function (response) {
         console.log(response);
+        appendToDoList(response);
     }).catch(function (error) {
         console.log('error in GET', error);
     });
 } //end GET getList
+
+function appendToDoList(todolist) {
+    $('#domTaskList').empty();
+
+
+    for (let i = 0; i < todolist.length; i++) {
+        let task = todolist[i];
+
+        let row = $(`
+        <tr>
+            <td>${task.task}</td>
+            <td>${task.type}</td>
+            <td>${task.notes}</td>
+            <td>${task.completed}</td>
+            <td><button class="completeButton">Completed task?</button><button class="deleteButton">Delete?</button></td>
+        </tr>
+        `);
+        row.data('id', task.id);
+        $('#domTaskList').append(row);
+    }
+};
